b7e9be040e714077ef203c0d92f63c6016f3f4d5
hilmi3x <hilmi_58_araz@hotmail.com>
2015-04-29 15:31:52 +0300

    Resolution
    Changed to windowed mode

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -12,7 +12,7 @@ import States.Options;
 
 public class Game extends StateBasedGame {
 	public static final String name = "Kitten Maxit";
-	public static final int width = 1366, height = 768;
+	public static final int width = 800, height = 600;
 	public static int menu = 0, level1 = 1, options = 2, credits = 3;
 
 	public Game(String name) {
@@ -37,7 +37,7 @@ public class Game extends StateBasedGame {
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app;
-		app = new AppGameContainer(new Game(name), width, height, true);
+		app = new AppGameContainer(new Game(name), width, height, false);
 		app.setVSync(true);
 		app.start();
 	}
